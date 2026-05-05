df5cb465c79dda2875cd5fb955b3eff590e5edc8
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2026-05-05 16:02:30 +0300

    add schemas from schemastore for integration testing

--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,5 @@ import Config
 
 config :rock_solid, client_options: [plug: {Req.Test, RockSolid.Client}, retry: false]
 config :logger, :default_handler, false
+
+config :ex_unit, exclude: [:regression]
